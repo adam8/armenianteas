8d0b6433fe0915f0ecaa3dfbd4defbc5cb77473c
Adam Skye Jones <adamjones.ca@gmail.com>
2024-11-18 16:38:25 -0800

    add html content-type header

--- functions/hola.js
+++ functions/hola.js
@@ -1,5 +1,10 @@
 import { header } from "../header.js";
 
 export async function onRequest(context) {
-    return new Response(`${header("Pages Functions")} <div>hola</div></body></html>`);
+    return new Response(
+        `${header("Pages Functions")} <div>hola</div></body></html>`,
+        {
+            headers: { "Content-Type": "text/html" }
+        }
+    );
 }
